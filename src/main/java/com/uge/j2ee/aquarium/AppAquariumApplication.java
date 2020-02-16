ce450b49aa25b32e2bb7afff6c19faa2987e0fdf
Ylenia Galluzzo <yleniagalluzzo1@gmail.com>
2020-02-16 18:16:13 +0100

    Initial commit + resolve bug configuration prec project (start create environment for backend, frontend part)

---
+++ src/main/java/com/uge/j2ee/aquarium/AppAquariumApplication.java
@@ -0,0 +1,15 @@
+package com.uge.j2ee.aquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class AppAquariumApplication {
+
+	public static void main(String[] args) {SpringApplication.run(AppAquariumApplication.class, args);
+	}
+
+}

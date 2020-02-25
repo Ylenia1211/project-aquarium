42065136ed00c9ef2928f9630d97b6ba56ba5ab8
Ylenia Galluzzo <yleniagalluzzo1@gmail.com>
2020-02-25 19:04:39 +0100

    add login component method check

--- src/main/angular/src/app/login/login.component.ts
+++ src/main/angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = 'root';
+  password = '';
+  invalidLogin = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private loginservice: AuthenticationService) { }
+
+  ngOnInit() {
+  }
+
+  checkLogin() {
+    if (this.loginservice.authenticate(this.username, this.password)) {
+      this.router.navigate(['']);
+      this.invalidLogin = false
+    } else
+      this.invalidLogin = true
   }
 
 }

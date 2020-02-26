d355c17df44fe2e0a9c3dbcf01fb63477ee19dd4
Ylenia Galluzzo <yleniagalluzzo1@gmail.com>
2020-02-26 11:47:25 +0100

    service authentication

--- src/main/angular/src/app/authentication.service.ts
+++ src/main/angular/src/app/authentication.service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthenticationService {
+  //sessionStorage propreties object stores data for only one session
 
+  constructor() { }
+
+  authenticate(username, password) {
+    if (username === "root" && password === "root") {
+      sessionStorage.setItem('username', username);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('username');
+    console.log(!(user === null));
+    return !(user === null)
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username')
+  }
 
 }
